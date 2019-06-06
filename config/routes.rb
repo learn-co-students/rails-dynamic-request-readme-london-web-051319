3f831a330d9256bdc714f4cd874f91dd9700e10b
github username <github email address>
2019-06-06 10:19:08 +0100

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,8 @@ Rails.application.routes.draw do
   get 'welcome', to: 'static#welcome'
 #goes to my static welcome page
 
-get 'posts/:id', to: 'posts#show'
+# get 'posts/:id', to: 'posts#show'
+
+   resources :posts, only: :show
+
 end
